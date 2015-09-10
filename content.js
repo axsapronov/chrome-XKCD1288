c0766096768bb79b6e58835ea15f3ed9f42eecf8
Sapronov Alexander <sapronov.alexander92@gmail.com>
2015-09-10 12:10:26 +0600

    Init

---
+++ content.js
@@ -0,0 +1,28 @@
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+
+var XKCD_dict = {
+    "свидетели": "знакомые чуваки",
+    "предположительно": "вроде как",
+    "новые исследования": "пост на tumblr'е",
+    "восстановить": "отомстить за",
+    "космос": "кооосмос",
+    "google glass": "virtual boy",
+    "смартфон": "тамагочи",
+    "электрический": "атомный",
+    "депутат": "эльфийский лорд",
+    "автомобиль": "кот",
+    "выборы": "еда на скорость",
+    "лидеры партии": "духи реки",
+    "анб": "квн",
+    "отказался давать комментарий": "виновен, и все прекрасно знают"
+}
+
+$(document).ready(function() {
+    var replaced = document.body.innerHTML;
+    $.each(XKCD_dict, function( key, value) {
+        var regEx = "(" + key + ")(?!([^<]+)?>)"
+        replaced = replaced.replace(new RegExp(regEx, "ig"), value.toUpperCase());  
+    })
+    document.body.innerHTML = replaced
+});
